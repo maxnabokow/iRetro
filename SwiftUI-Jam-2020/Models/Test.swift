f11a12149b301415c214bbe8a5b10fe72ce51332
Maximilian Nabokow <max.nabokow@gmail.com>
2021-02-19 15:12:40 -0500

    Filling folders

---
+++ SwiftUI-Jam-2020/Models/Test.swift
@@ -0,0 +1,8 @@
+//
+//  Test.swift
+//  SwiftUI-Jam-2020
+//
+//  Created by Max Nabokow on 2/19/21.
+//
+
+import Foundation
---
+++ SwiftUI-Jam-2020/ViewModels/Test1.swift
@@ -0,0 +1,8 @@
+//
+//  Test1.swift
+//  SwiftUI-Jam-2020
+//
+//  Created by Max Nabokow on 2/19/21.
+//
+
+import Foundation

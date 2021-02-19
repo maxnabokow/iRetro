7850e4dcac7ffbe05004143a92f110f9d41bc22c
Maximilian Nabokow <max.nabokow@gmail.com>
2021-02-19 12:16:08 -0500

    Added blank Xcode Project

---
+++ SwiftUI-Jam-2020/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Jam-2020
+//
+//  Created by Max Nabokow on 2/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

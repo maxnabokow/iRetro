40ed34d101cd8f55547bd3684cffc647cb50cd35
Andreas Ink <andreasink17@gmail.com>
2021-02-20 12:27:35 -0500

    Implemented artwork to UIImage

--- SwiftUI-Jam-2020/Views/NowPlayingView.swift
+++ SwiftUI-Jam-2020/Views/NowPlayingView.swift
@@ -12,11 +12,25 @@ struct NowPlayingView: View {
     @Environment(\.presentationMode) private var presentationMode
 
     var body: some View {
+        GeometryReader { proxy in
+            VStack {
+                iPodStatusBar()
+        HStack {
+            Image(uiImage: vm.nowPlayingItem?.artwork?.image(at: CGSize(width: proxy.size.width/4, height: proxy.size.width/4)) ?? UIImage(named: "abbeyRoad")!)
+                
+               
+                .scaledToFit()
         Text(vm.nowPlayingItem?.title ?? "NO TITLE")
             .navigationBarHidden(true)
             .onAppear(perform: startNowPlayingSubscriptions)
             .onAppear(perform: startClickWheelSubscriptions)
             .onDisappear(perform: stopClickWheelSubscriptions)
+            .padding()
+        }
+                
+
+            }
+    }
     }
 
     private func startNowPlayingSubscriptions() {

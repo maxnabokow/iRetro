402226d2dca981de211e9490d66a70ff0054dda0
Maximilian Nabokow <max.nabokow@gmail.com>
2021-03-14 15:18:50 -0400

    Improved UserData

--- SwiftUI-Jam-2020/UserData.swift
+++ SwiftUI-Jam-2020/UserData.swift
@@ -5,35 +5,12 @@
 //  Created by Andreas on 2/21/21.
 //
 
-import Foundation
 import SwiftUI
-import Combine
 
 final class UserData: ObservableObject {
-    
-    public static let shared = UserData()
-    
-    @Published(key: "firstRun")
-    var firstRun: Bool = true
-    
-    @Published(key: "isOnboardingCompleted")
-    var isOnboardingCompleted: Bool = false
-    
-   
-}
-import Foundation
-import Combine
+    static let shared = UserData()
+    private init() {}
 
-extension Published {
-    
-    init(wrappedValue defaultValue: Value, key: String) {
-        let value = UserDefaults.standard.object(forKey: key) as? Value ?? defaultValue
-        self.init(initialValue: value)
-        projectedValue.receive(subscriber: Subscribers.Sink(receiveCompletion: { (_) in
-            ()
-        }, receiveValue: { (value) in
-            UserDefaults.standard.set(value, forKey: key)
-        }))
-    }
-    
+    @AppStorage("firstRun") var firstRun: Bool = true
+    @AppStorage("isOnboardingCompleted") var isOnboardingCompleted: Bool = false
 }

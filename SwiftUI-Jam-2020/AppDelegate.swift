c0568cdd67b5bc9e5cadc18216593af4553a2f2e
Andreas Ink <andreasink17@gmail.com>
2021-03-14 12:47:55 -0400

    Removed testing function

--- SwiftUI-Jam-2020/AppDelegate.swift
+++ SwiftUI-Jam-2020/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        MusicManager.shared.playFavoriteSong()
+       
         return true
     }
 
